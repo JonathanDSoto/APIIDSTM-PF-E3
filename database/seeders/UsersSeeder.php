d572d7124d51a2d733fb1e02db2f786a4108cbe9
Christian Duarte <w.christian.duarte@gmail.com>
2023-11-23 20:16:45 -0700

    Agregado seeder de usuarios

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $content = json_decode(file_get_contents('database/data/users.json'), true);
+        foreach ($content as $user) {
+            User::create($user);
+        }
+    }
+}
