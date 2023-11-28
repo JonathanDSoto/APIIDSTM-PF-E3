19d8c82eee15b32b1d9d79fb40f0331def7fe17b
Israel Santamaria Zarate <israelz961@gmail.com>
2023-11-27 20:14:30 -0700

    Arreglos de diseño en categorias

--- resources/js/pages/CategoriesPage.vue
+++ resources/js/pages/CategoriesPage.vue
@@ -1,16 +1,28 @@
 <script setup>
 import NonoBreadcrums from '../components/NonoBreadcrums.vue';
+import CategoriesModal from '../components/categories/CategoriesModal.vue';
 import DataTable from 'datatables.net-vue3';
 import DataTablesCore from 'datatables.net';
 import { getCategories } from '../api/categories';
 DataTable.use(DataTablesCore);
 const DataTableoptions = {
-    serverSide: true
+    serverSide: true,
+    createdRow: (e) => {
+        e.getElementsByTagName('td').forEach(e => e.classList.add('table-td'))
+    },
+    dom: "<'grid grid-cols-12 gap-5 px-6 mt-6'<'col-span-4'l><'col-span-8 flex justify-end'f><'#pagination.flex items-center'>><'min-w-full't><'flex justify-end items-center'p>",
+    language: {
+        "lengthMenu": "Show _MENU_ entries",
+        "paginate": {
+            "previous": "<iconify-icon icon=\"ic:round-keyboard-arrow-left\"></iconify-icon>",
+            "next": "<iconify-icon icon=\"ic:round-keyboard-arrow-right\"></iconify-icon>"
+        },
+    },
 }
 const DataTabledata = [
-    {data:"id"},{data:"name"},
-    {data: null,render: function ( data, type, row, meta ) {
-  return 'Acciones';
+{data:"id"},{data:"name"},
+{data: null,render: function ( data, type, row, meta ) {
+    return 'Acciones';
 }}
 
 ]
@@ -18,10 +30,9 @@ const DataTableajax = (data, callback) => {
     getCategories(parseInt(data.start/data.length)+1).then((categories) => {
         callback({
             draw: data.draw,
-            recordsTotal: categories.total,recordsFiltered: categories.total,
             data:categories.data,
-            recordsTotal: categories.total,
-            recordsFiltered: categories.total,
+            recordsTotal: categories.meta.total,
+            recordsFiltered: categories.meta.total,
         })
     })
 }
@@ -36,7 +47,7 @@ const DataTableajax = (data, callback) => {
             <header class=" card-header noborder">
                 <h4 class="card-title">Categorias
                 </h4>
-                <button class="btn inline-flex justify-center btn-success">Añadir categoria</button>
+                <button class="btn inline-flex justify-center btn-success" data-bs-toggle="modal" data-bs-target="#categoriesmodal">Añadir categoria</button>
             </header>
             <div class="card-body px-6 pb-6">
                 <div class="overflow-x-auto -mx-6 dashcode-data-table">
@@ -46,61 +57,36 @@ const DataTableajax = (data, callback) => {
                         <div class="overflow-hidden ">
                             <DataTable class="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700"
                             :ajax="DataTableajax" :options="DataTableoptions" :columns="DataTabledata" >
-                                <thead class="border-t border-slate-100 dark:border-slate-800">
-                                    <tr>
+                            <thead class="border-t border-slate-100 dark:border-slate-800">
 
-                                        <th scope="col" class=" table-th w-[100px] ">
-                                            Id
-                                        </th>
+                                <tr>
 
-                                        <th scope="col" class=" table-th ">
-                                            Nombre
-                                        </th>
+                                    <th scope="col" class=" table-th w-[100px] ">
+                                        Id
+                                    </th>
 
-                                        <th scope="col" class=" table-th w-[100px] ">
-                                            Accion
-                                        </th>
+                                    <th scope="col" class=" table-th ">
+                                        Nombre
+                                    </th>
 
-                                    </tr>
-                                </thead>
-                            </DataTable>
-                        </div>
+                                    <th scope="col" class=" table-th !w-[100px] ">
+                                        Accion
+                                    </th>
+
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
+
+                            </tbody>
+                        </DataTable>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="modal_editar" tabindex="-1" aria-labelledby="modal_editar" aria-hidden="true">
-        <div class="modal-dialog top-1/2 !-translate-y-1/2 relative w-auto pointer-events-none">
-            <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-                <div class="relative bg-white rounded-lg shadow dark:bg-slate-700">
-                    <!-- Modal header -->
-                    <div class="flex items-center justify-between p-5 rounded-t dark:border-slate-600 bg-primary-500">
-                        <h3 class="text-xl font-medium text-white dark:text-white capitalize">
-                            Editar Categoria
-                        </h3>
-                        <button type="button" class="text-slate-400 bg-transparent hover:text-slate-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-slate-600 dark:hover:text-white" data-bs-dismiss="modal">
-                            <svg aria-hidden="true" class="w-5 h-5" fill="#ffffff" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                            </svg>
-                            <span class="sr-only">Close modal</span>
-                        </button>
-                    </div>
-                    <!-- Modal body -->
-                    <div class="p-6 space-y-4">
-                        <div class="input-area">
-                            <label for="name" class="form-label">Nombre*</label>
-                            <input required id="name" type="text" class="form-control" placeholder="Nombre">
-                        </div>
-                    </div>
-                    <!-- Modal footer -->
-                    <div class="flex justify-between items-center p-6 space-x-2 border-t border-slate-200 rounded-b dark:border-slate-600">
-                        <button data-bs-dismiss="modal" class="btn inline-flex justify-center btn-danger">Cancelar</button>
-                        <button data-bs-dismiss="modal" class="btn inline-flex justify-center btn-success">Guardar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+</div>
+<CategoriesModal>
+
+</CategoriesModal>
 </template>
 
---
+++ resources/js/pages/EtiquetasPage.vue
@@ -0,0 +1,8 @@
+<script setup>
+import NonoBreadcrums from '../components/NonoBreadcrums.vue';
+import DataTable from 'datatables.net-vue3';
+import DataTablesCore from 'datatables.net';
+</script>
+<template>
+
+</template>
