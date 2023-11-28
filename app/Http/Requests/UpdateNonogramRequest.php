deb8c51a08b42bcf9d88373caa2d3214d1c75159
Christian Duarte <w.christian.duarte@gmail.com>
2023-11-28 10:22:28 -0700

    Require data if resized

--- app/Http/Requests/UpdateNonogramRequest.php
+++ app/Http/Requests/UpdateNonogramRequest.php
@@ -35,7 +35,7 @@ class UpdateNonogramRequest extends FormRequest
             'colors.*' => [ 'required', 'string', 'regex:/^(rgb\((\s*([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5])\,){2}(\s*([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5]))\s*\))|(rgba\((\s*([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5])\,){3}\s*([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5])\s*\))|(#[0-9A-Fa-f]{6})|(#[0-9A-Fa-f]{3})$/' ],
             'width' => [ 'sometimes', 'required', 'integer' ],
             'height' => [ 'sometimes', 'required', 'integer' ],
-            'data' => [ 'sometimes', 'required', 'array', filter_var($width, FILTER_VALIDATE_INT) ? 'size:' . $width  : ''],
+            'data' => [ 'required_with:width', 'required_with:height', 'array', filter_var($width, FILTER_VALIDATE_INT) ? 'size:' . $width  : ''],
             'data.*' => [ 'required', 'array', filter_var($height, FILTER_VALIDATE_INT) ? 'size:' . $height : '' ],
             'data.*.*' => [ 'required', 'integer', 'min:0', 'max:' . (count($colors) - 1) ],
             'category_id' => [ 'sometimes', 'required', 'exists:categories,id' ],
