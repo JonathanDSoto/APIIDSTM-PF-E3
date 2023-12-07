5eafcab3d63bef5bf70a9f66ff36bf93f7b875e7
Israel Santamaria Zarate <israelz961@gmail.com>
2023-12-07 00:03:12 -0700

    modulo de etiquetas terminado

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue/dist/vue.esm-bundler';
 import './bootstrap';
 import CategoriesPage from "./pages/CategoriesPage.vue"
 import DifficultiesPage from "./pages/DifficultiesPage.vue"
+import TagsPage from "./pages/TagsPage.vue"
 createApp({
     components:{
         CategoriesPage,
-        DifficultiesPage
+        DifficultiesPage,
+        TagsPage,
     }
 }).mount("#content_layout")
--- resources/js/pages/EtiquetasPage.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import NonoBreadcrums from '../components/NonoBreadcrums.vue';
-import DataTable from 'datatables.net-vue3';
-import DataTablesCore from 'datatables.net';
-</script>
-<template>
-
-</template>
