f4d8852682ecf5c07fd217e0c28f649d4cb5f62e
Julio14sk <jvaldovino_20@alu.uabcs.mx>
2023-12-07 02:16:07 -0700

    Pestaña de Nonogramas Terminado

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,12 @@ import './bootstrap';
 import CategoriesPage from "./pages/CategoriesPage.vue"
 import DifficultiesPage from "./pages/DifficultiesPage.vue"
 import TagsPage from "./pages/TagsPage.vue"
+import NonogramsPage from "./pages/NonogramsPage.vue"
 createApp({
     components:{
         CategoriesPage,
         DifficultiesPage,
         TagsPage,
+        NonogramsPage,
     }
 }).mount("#content_layout")
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-        }), vuePlugin()
+        }), vuePlugin({
+            template:{
+                compilerOptions:{
+                    isCustomElement: (tag) => tag.startsWith('nonogram-')
+                }
+            }
+        })
     ],
 });
