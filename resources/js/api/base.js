2ccaa7db88292f3fc9f3614256fc7b66b0d43e57
Christian Duarte <w.christian.duarte@outlook.com>
2023-12-08 07:17:00 -0700

    Nonograms url added

--- resources/js/api/base.js
+++ resources/js/api/base.js
@@ -1,5 +1,5 @@
 import _axios from 'axios';
 const axios = _axios;
-axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.baseURL = 'https://nonograms.dododev.net/api/';
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 export default axios;
